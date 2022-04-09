8adc1ded15d05d22b9ba97dbe5511599fc41d47f
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-09 15:36:35 +0200

    Edit a task, remove a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,16 +7,38 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # /tasks/new
   def new
     #  display form with empty fields
     @task = Task.new
   end
 
+  # /tasks/new
   def create
     # create new task with details entered by user
     @task = Task.new(task_params)   # (params[:task]) this will raise ActiveModel::ForbiddenAttributesError
     @task.save
     redirect_to task_path(@task.id) # show task
+  end
+
+  # /tasks/:id/edit
+  def edit
+    # display form with filled in fields
+    @task = Task.find(params[:id])
+  end
+
+  # /tasks/:id/edit
+  def update
+    #  find the Task to be edited, update values & redirect to details page (show)
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id) # show task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  # As a user, I can list tasks
+  # READ - As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
-  # As a user, I can add a new task
+  # CREATE - As a user, I can add a new task
   # One route is there to display the Task form, and
   # the other is there to handle the POST request generated when submitting this form
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
+  # UPDATE As a user, I can edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE As a user, I can remove a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
   # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
