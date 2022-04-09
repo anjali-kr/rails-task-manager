4cf1a757244b040726b1eb94aa29b1845b56e568
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-09 13:34:42 +0200

    Add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    #  display form with empty fields
+    @task = Task.new
+  end
+
+  def create
+    # create new task with details entered by user
+    @task = Task.new(task_params)   # (params[:task]) this will raise ActiveModel::ForbiddenAttributesError
+    @task.save
+    redirect_to task_path(@task.id) # show task
+  end
+
+  private
+
+  # Strong Params
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   # As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+  # As a user, I can add a new task
+  # One route is there to display the Task form, and
+  # the other is there to handle the POST request generated when submitting this form
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
